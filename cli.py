8492d5c33848c7982569735ca58fae1875cb5df7
Jonatas Oliveira <jonatas.oliveira@gmail.com>
2011-07-30 17:45:49 -0300

    Adding docstring params.

--- cli.py
+++ cli.py
@@ -28,6 +28,14 @@ class ConnectXMLRPC(object):
 
 
 class Domain(ConnectXMLRPC):
+    def _kwargs_to_args_ordenated(self, **kwargs):
+        # The XML-RPC doesn't accept named params. The keys are
+        # to ordenate the params and use as **params.
+        keys = ['domain', 'domain_active', 'soa_ttl', 'soa_serial',
+                'soa_refresh_seconds', 'soa_retry', 'soa_expire',
+                'soa_minimum', 'group_id', 'domain_linked_id']
+        return [kwargs[k] for k in keys]
+
     def list(self, limit=False):
         """
         List all domains, 
@@ -47,26 +55,52 @@ class Domain(ConnectXMLRPC):
     def create(self, *args, **kwargs):
         """
         Create a new domain.
+
+        @param domain
+        @param domain_active
+        @param soa_ttl
+        @param soa_serial
+        @param soa_refresh_seconds
+        @param soa_retry
+        @param soa_expire
+        @param soa_minimum
+        @param group_id
+        @param domain_linked_id
         """
         try:
-            # The XML-RPC doesn't accept named params. The keys are
-            # to ordenate the params and use as **params.
-            keys = ['domain', 'domain_active', 'soa_ttl', 'soa_serial',
-                    'soa_refresh_seconds', 'soa_retry', 'soa_expire',
-                    'soa_minimum', 'group_id', 'domain_linked_id']
-            params = [kwargs[k] for k in keys]
+            params = self._kwargs_to_args_ordenated(**kwargs)
             result = self.client.domain.create(*params)
         except socket.error, msg:
             show_error(msg, exit=True)
-        #except Exception, msg:
-        #    show_error(msg, exit=True)
         
         if result == True:
             msg = 'SUCCESS! The domain %s was created.' % kwargs['domain']
             show_message(msg, exit=True)
     
-    def update(self, args):
-        print '>>> CREATE: %s %s' % (args.dirname, args.read_only)
+    def update(self, *args, **kwargs):
+        """
+        Update a domain.
+
+        @param domain
+        @param domain_active
+        @param soa_ttl
+        @param soa_serial
+        @param soa_refresh_seconds
+        @param soa_retry
+        @param soa_expire
+        @param soa_minimum
+        @param group_id
+        @param domain_linked_id
+        """
+        try:
+            params = self._kwargs_to_args_ordenated(**kwargs))
+            result = self.client.domain.update(*params)
+        except socket.error, msg:
+            show_error(msg, exit=True)
+    
+        if result == True:
+            msg = 'SUCCESS! The domain %s was updated.' % kwargs['domain']
+            show_message(msg, exit=True)
     
     def delete(self, args):
         print '>>> DELETE: %s' % args
@@ -98,6 +132,19 @@ domain_create_parser.add_argument('--group', dest='group_id', default=None, help
 domain_create_parser.add_argument('--linked-to', dest='domain_linked_id', default=None, help='Domain link')
 domain_create_parser.set_defaults(cls='domain', method='create')
 
+# Domain update parser
+domain_create_parser = subparsers.add_parser('domainupdate', help='Update domain')
+domain_create_parser.add_argument('domain', help='Domain name')
+domain_create_parser.add_argument('--active', dest='domain_active', default=None, help='Active domain?')
+domain_create_parser.add_argument('--soa-ttl', dest='soa_ttl', default=None, help='SOA TTL')
+domain_create_parser.add_argument('--soa-serial', dest='soa_serial', default=None, help='SOA serial')
+domain_create_parser.add_argument('--soa-refresh-time', dest='soa_refresh_seconds', default=None, help='SOA refresh time')
+domain_create_parser.add_argument('--soa-retry-time', dest='soa_retry', default=None, help='SOA retry time')
+domain_create_parser.add_argument('--soa-expire-time', dest='soa_expire', default=None, help='SOA expire time')
+domain_create_parser.add_argument('--soa-minimum-ttl', dest='soa_minimum', default=None, help='SOA minimum TTL')
+domain_create_parser.add_argument('--group', dest='group_id', default=None, help='Domain group')
+domain_create_parser.add_argument('--linked-to', dest='domain_linked_id', default=None, help='Domain link')
+domain_create_parser.set_defaults(cls='domain', method='update')
 
 # Getting the args.
 args = parser.parse_args()
