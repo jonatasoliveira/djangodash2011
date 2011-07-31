0aadccece91f1f31c6a5d083656ee2f9919fb4bf
root <root@search01.integrado.ws>
2011-07-31 19:41:27 -0300

    Done with domain create, udpate and delete.

--- pymandns/control/urls.py
+++ pymandns/control/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('control.views',
     url(r'^domain/create/?$', 'domain_create', name='domain_create'),
     url(r'^domain/list/?$', 'domain_list', name='domain_list'),
     url(r'^domain/edit/(?P<domain_name>.+)/?$', 'domain_edit', name='domain_edit'),
+    url(r'^domain/delete/(?P<domain_name>.+)/?$', 'domain_delete', name='domain_delete'),
 )
