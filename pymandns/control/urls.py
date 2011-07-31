d84b454ea037136cea8f07d86d9c81206a7b071c
root <root@search01.integrado.ws>
2011-07-31 16:18:44 -0300

    View para criar dominio.

--- pymandns/control/urls.py
+++ pymandns/control/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('control.views',
     # Example:
-    url(r'^/?$', 'dashboard', name='dashboard'),
+    url(r'^$', 'dashboard', name='dashboard'),
+    url(r'^domain/create/?$', 'domain_create', name='domain_create'),
 )
--- pymandns/control/views.py
+++ pymandns/control/views.py
@@ -1,9 +1,27 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
+from django.contrib import messages
+
+from forms import *
+from models import *
 
 def dashboard(request):
     return render_to_response('dashboard.html', locals(),
         context_instance=RequestContext(request))
 
+def domain_create(request):
+
+    if request.method == 'POST':
+        domain_form = DomainForm(request.POST)
+        if domain_form.is_valid():
+            #import pdb; pdb.set_trace()
+            messages.success(request, u'The domains %s was created.' % domain_form.cleaned_data['domain'])
+            return redirect('dashboard')
+        pass
+    else:
+        domain_form = DomainForm()
+
+    return render_to_response('domain_create.html', locals(),
+        context_instance=RequestContext(request))
 
