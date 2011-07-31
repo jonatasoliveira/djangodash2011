0de7fc62a329bff3ea744d1c24e8679dd063ff17
root <root@search01.integrado.ws>
2011-07-31 19:40:31 -0300

    Minimal correction.

--- pymandns/control/forms.py
+++ pymandns/control/forms.py
@@ -7,7 +7,7 @@ class DomainForm(forms.Form):
     domain_active = forms.BooleanField(label=u'Domain active?', initial=True, required=False)
     soa_ttl = forms.IntegerField(label=u'TTL')
     soa_serial = forms.IntegerField(label=u'Serial')
-    soa_refresh_seconds = forms.IntegerField(label=u'Refresh time')
+    soa_refresh = forms.IntegerField(label=u'Refresh time')
     soa_retry = forms.IntegerField(label=u'Retry time')
     soa_expire = forms.IntegerField(label=u'Expire time')
     soa_minimum = forms.IntegerField(label=u'Minimum time')
